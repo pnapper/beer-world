9b5e4fd137abe85d20b899f851896a57122b84f9
Andy Luc, Patrick Napper and Tram Dinh <tram@>
2017-07-19 19:34:52 -0700

    fix business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Business logic
-function Beer (color, bitterness, level, price, hop, origin) {
+function Beer (color, bitterness, level, price) {
   this.color = color;
   this.bitterness = bitterness;
   this.level = level;
@@ -11,7 +11,7 @@ function Beer (color, bitterness, level, price, hop, origin) {
 //Interface logic
 $(document).ready(function(){
   $("#submit").click(function() {
-    var beer = new Beer();
+
     var color = $("input:radio[name=color]:checked").val();
     console.log(color)
     var bitterness = $("input:radio[name=bitter]:checked").val();
@@ -20,11 +20,11 @@ $(document).ready(function(){
     console.log(level)
     var price = $("input:radio[name=price]:checked").val();
     console.log(price)
-
+    var beer = new Beer(color, bitterness, level, price);
     $("input:checkbox[id=hop]:checked").each(function() {
       var hop = $(this).val();
       beer.hop.push(hop);
-      console.log(this.hop)
+      console.log(hop)
     });
 
     $("input:checkbox[id=origin]:checked").each(function() {
