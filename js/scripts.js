7a335b07ee63b349db5522252ad4024d00052d7e
Andy Luc, Patrick Napper and Tram Dinh <patrick@>
2017-07-19 19:26:57 -0700

    Create constructor and object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
+//Business logic
+function Beer (color, bitterness, level, price, hop, origin) {
+  this.color = color;
+  this.bitterness = bitterness;
+  this.level = level;
+  this.price = price;
+  this.hop = [];
+  this.origin = [];
+}
+
 //Interface logic
 $(document).ready(function(){
   $("#submit").click(function() {
+    var beer = new Beer();
+    var color = $("input:radio[name=color]:checked").val();
+    console.log(color)
+    var bitterness = $("input:radio[name=bitter]:checked").val();
+    console.log(bitterness)
+    var level = $("input:radio[name=drunk]:checked").val();
+    console.log(level)
+    var price = $("input:radio[name=price]:checked").val();
+    console.log(price)
+
+    $("input:checkbox[id=hop]:checked").each(function() {
+      var hop = $(this).val();
+      beer.hop.push(hop);
+      console.log(this.hop)
+    });
+
+    $("input:checkbox[id=origin]:checked").each(function() {
+      var origin = $(this).val();
+      beer.origin.push(origin);
+      console.log(origin)
+    });
+    console.log(beer)
+
     $("#survey").hide();
     $("#return").show();
   });
