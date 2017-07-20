dd0074aaf420f9f7c0cfc1c8308850cfdb3f28db
Andy Luc, Patrick Napper and Tram Dinh <tram@>
2017-07-19 20:13:28 -0700

    add to README

--- README.md
+++ README.md
@@ -10,3 +10,46 @@ Goal: Create a webpage that helps novice beer drinkers find a beer style they ma
    - Beer origin (Europe, Domestic, Asia)
 
 Return: Lager, IPA, Amber Ale, Red Ale, Stout, Cider
+
+Lager:
+  Color: light
+  Bitterness: none
+  Level: < 4%
+  Price: <= $4
+  Hop: n/a
+  Origin: Europe, Domestic, Asia
+IPA:
+  Color: light, golden
+  Bitterness: medium, heavy
+  Level: 5-7%, 8-10%
+  Price: $5-8
+  Hop: Pine, Floral, Citrus
+  Origin: Domestic
+Amber Ale:
+  Color: amber
+  Bitterness: light, medium
+  Level: 5-7%
+  Price: $5-8
+  Hop: n/a
+  Origin: Domestic, Europe
+Red Ale:
+  Color: red
+  Bitterness: medium
+  Level: 5-7%
+  Price: $5-8
+  Hop: n/a
+  Origin: Domestic, Europe
+Porter:
+  Color: brown
+  Bitterness: light, medium
+  Level: 5-7%, >10%
+  Price: $5-8, > $8
+  Hop: n/a
+  Origin: Domestic, Europe
+Stout:
+  Color: dark
+  Bitterness: light
+  Level: 5-7%, >10%
+  Price: $5-8, > $8
+  Hop: n/a
+  Origin: Domestic, Europe
--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,28 @@ function Beer (color, bitterness, level, price) {
   this.origin = [];
 }
 
+prototype.beer.choice function() {
+
+}
+
 //Interface logic
 $(document).ready(function(){
   $("#submit").click(function() {
 
     var color = $("input:radio[name=color]:checked").val();
-    console.log(color)
     var bitterness = $("input:radio[name=bitter]:checked").val();
-    console.log(bitterness)
     var level = $("input:radio[name=drunk]:checked").val();
-    console.log(level)
     var price = $("input:radio[name=price]:checked").val();
-    console.log(price)
     var beer = new Beer(color, bitterness, level, price);
     $("input:checkbox[id=hop]:checked").each(function() {
       var hop = $(this).val();
       beer.hop.push(hop);
-      console.log(hop)
     });
 
     $("input:checkbox[id=origin]:checked").each(function() {
       var origin = $(this).val();
       beer.origin.push(origin);
-      console.log(origin)
     });
-    console.log(beer)
 
     $("#survey").hide();
     $("#return").show();
